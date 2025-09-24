a836c88b2b346dfba1ff57b4ddb4fa3a5914f79f
vanevila27 <66636851+vanevila27@users.noreply.github.com>
2025-09-23 23:46:04 -0300

    Primer commit: subiendo mi proyecto

---
+++ img/script.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        merriweather: ['Merriweather', 'serif'], // ¡Nueva fuente para el título!
+      },
+    },
+  },
+  plugins: [],
+}
